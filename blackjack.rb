b4bb16258ba26b91f4ad196975e35504e16c1b64
Grey Elerson <grey.l.elerson@gmail.com>
2015-01-03 03:07:32 +0000

    total value of hand functionality

--- blackjack.rb
+++ blackjack.rb
@@ -1,4 +1,5 @@
-# ========================================================= Pseudocode Algorithm
+require 'pry'
+# ============================================================================== Pseudocode Algorithm
 
 # 1. Prepare the decks. Generate 52 unique cards. Repeat for cases with more than
 #    1 deck. Shuffle all the cards.
@@ -23,10 +24,67 @@
 # - Face cards (K, Q, J) are worth 10
 # - All number cards are worth their number, regardless of suit
 
-# =========================================================== Method Definitions
+# ============================================================================== Method Definitions
 
+def total_with_aces(subtotal, number_of_aces_in_hand) # <= Integer, Integer
+  return 100 if (subtotal + number_of_aces_in_hand > 21)
+  case number_of_aces_in_hand
+    when 0
+      total = subtotal
+    when 1
+      total = (subtotal + 11 > 21) ? (subtotal + 1) : (subtotal + 11)
+    when 2
+      total = (subtotal + 12 > 21) ? (subtotal + 2) : (subtotal + 12)
+    when 3
+      total = (subtotal + 13 > 21) ? (subtotal + 3) : (subtotal + 13)
+    when 4
+      total = (subtotal + 14 > 21) ? (subtotal + 4) : (subtotal + 14)
+  end
+  total
+end # => Integer
 
+def total_of_hand(hand) # <= Array
+  total = 0
+  
+  # move all aces to the end of the array
+  aces = ["[♠ A]","[♣ A]","[♥ A]","[♦ A]"]
+  indices_of_aces = hand.each_index.select { |i| aces.include? hand[i] }
+  
+  
+  hand.each do |card|
+    face = card.split("")[3]
+    value = face.to_i if %w(1 2 3 4 5 6 7 8 9).include? face
+    value = 10 if %w(Q K J).include? face
+    value = 0 if face == "A"
+    total += value
+  end
+  
+  total = total_with_aces(total, indices_of_aces.count)
+  
+  return total, hand
+end # => Integer
 
-# =================================================================== Game Logic
+def prepare_decks(number_of_decks) # <= Integer
+  suits = ["♠","♣","♥","♦"]
+  values = %w(1 2 3 4 5 6 7 8 9 J Q K A)
+  deck = []
+  
+  suits.each do |suit|
+    values.each do |value|
+      deck << "[#{suit} #{value}]"
+    end
+  end
+  
+  (deck *= number_of_decks).shuffle
+end # => Array
+
+5.times do
+  total, hand = total_of_hand(prepare_decks(1).sample(3))
+  puts hand.inspect
+  puts "total: #{total}"
+  puts ""
+end
+
+# ============================================================================== Game Logic
 
 
